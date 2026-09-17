2981e165a24f01779e3e2189cac677803bc05192
aingeru <aingerualvarezsoftware@gmail.com>
2026-09-17 15:10:24 +0200

    fix: Fixed no recognition of --help command and duplicate constant "name".
    Signed-off-by: aingeru <aingerualvarezsoftware@gmail.com>

--- src/constant.ml
+++ src/constant.ml
@@ -1,4 +1,4 @@
-let name: string = "ft_turing"
+let program_name: string = "ft_turing"
 let error: string = "Error"
 
 let read: string = "read"
@@ -13,9 +13,9 @@ let blank: string = "blank"
 let initial: string = "initial"
 let finals: string = "finals"
 
-let too_few_args: string = name ^ ": " ^ error ^ "; The program must have at least two arguments\n"
-let too_many_args: string = name ^ ": " ^ error ^ "; Too many arguments\n"
-let unrecognized_opt: string = name ^ ": " ^ error ^ "; Unrecognized option\n"
+let too_few_args: string = program_name ^ ": " ^ error ^ "; The program must have at least two arguments\n"
+let too_many_args: string = program_name ^ ": " ^ error ^ "; Too many arguments\n"
+let unrecognized_opt: string = program_name ^ ": " ^ error ^ "; Unrecognized option\n"
 let not_a_character: string = "must be a character"
 let not_valid_alphabet: string = "value is not a valid alphabet character"
 let must_not_be_blank: string = "value must not be the same as the blank character"
@@ -29,7 +29,7 @@ let decorator: string = "*******************************************************
 ********************************************************************************
 "
 
-let help_text: string = "usage: " ^ name ^ " [-h] jsonfile input
+let help_text: string = "usage: " ^ program_name ^ " [-h] jsonfile input
 
 positional arguments:
   jsonfile            json description of the machine
--- src/ft_turing.ml
+++ src/ft_turing.ml
@@ -17,17 +17,24 @@ let execute_args (fname: string) (input: string): int =
       0
     with
     | Type_error (msg, _) | Failure msg ->
-        Printf.eprintf "%s: %s; %s\n" Constant.name Constant.error msg;
+        Printf.eprintf "%s: %s; %s\n" Constant.program_name Constant.error msg;
         1
 
 let main () =
-  match Array.to_list Sys.argv with
-  | [] | [_] | [_; _] ->
-    Printf.eprintf "%s" Constant.too_few_args;
-    1
-  | [_; a2; a3] -> exit (execute_args a2 a3)
-  | _ -> 
-    Printf.eprintf "%s" Constant.too_many_args;
-    1
+  let args = Array.to_list Sys.argv in
+  if List.exists (fun arg -> arg = "--help" || arg = "-h") args then begin
+    Print.print_help ();
+    0
+  end else
+    match args with
+    | [_; a2; a3] ->
+        execute_args a2 a3
+    | _ when List.length args < 3 ->
+        Printf.eprintf "%s" Constant.too_few_args;
+        1
+    | _ ->
+        Printf.eprintf "%s" Constant.too_many_args;
+        1
 
 let () = exit (main ())
+
